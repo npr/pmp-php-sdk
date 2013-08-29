e066b88f224e9913249025a9570c92414cbb0a6e
Scott Smith <scottsmith130@gmail.com>
2013-08-29 10:13:53 -0400

    Implemented rels() method

--- lib/Pmp/CollectionDocJsonLinks.php
+++ lib/Pmp/CollectionDocJsonLinks.php
@@ -22,6 +22,19 @@ class CollectionDocJsonLinks
      * @return array
      */
     public function rels(array $urns) {
+        $count = count($urns);
+        $links = array();
 
+        foreach($this->links as $link) {
+
+            // array_diff gives elements of $urns that are not present in $link->rels,
+            // so if the result is not the same length as $urns, then we have a match
+            $result = array_diff($urns, $link->rels);
+            if (count($result) !== $count) {
+                $links[] = $link;
+            }
+        }
+
+        return $links;
     }
 }
