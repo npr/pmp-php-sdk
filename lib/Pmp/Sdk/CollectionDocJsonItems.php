f6a9175cbca9c59600efd0921fd00aae530e76cb
Scott Smith <scottsmith130@gmail.com>
2013-10-01 12:51:05 -0400

    CollectionDocJsonItems supports ArrayAccess interface

--- lib/Pmp/Sdk/CollectionDocJsonItems.php
+++ lib/Pmp/Sdk/CollectionDocJsonItems.php
@@ -3,7 +3,7 @@ namespace Pmp\Sdk;
 
 require_once('PageIterator.php');
 
-class CollectionDocJsonItems
+class CollectionDocJsonItems implements \ArrayAccess
 {
     public $_document;
     private $items;
@@ -41,5 +41,40 @@ class CollectionDocJsonItems
      */
     public function toArray() {
         return $this->items;
+    }
+
+    /**
+     * Required by the ArrayAccess interface
+     * @param mixed $offset
+     * @return bool
+     */
+    public function offsetExists($offset) {
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * Required by the ArrayAccess interface
+     * @param mixed $offset
+     * @return mixed
+     */
+    public function offsetGet($offset) {
+        return $this->items[$offset];
+    }
+
+    /**
+     * Required by the ArrayAccess interface
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset , $value) {
+        $this->items[$offset] = $value;
+    }
+
+    /**
+     * Required by the ArrayAccess interface
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset) {
+        unset($this->items[$offset]);
     }
 }
