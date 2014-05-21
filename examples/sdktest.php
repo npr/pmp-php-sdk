3da8205eb67b4be4e1aec7465ba6de569c39c455
Jason Grosman <jgrosman@npr.org>
2014-05-21 14:43:45 -0400

    Adding an examples directory for using the SDK

---
+++ examples/sdktest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Test that the SDK is available for use
+ *
+ * Arguments:
+ * -u Client Id
+ * -p Client Secret
+ */
+
+require_once(dirname(__FILE__) . '/../lib/Pmp/Sdk/AuthClient.php');
+
+echo "PMP API Test Completed.\n";
+exit(0);
