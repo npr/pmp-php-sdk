826a04b69847f0c265a8814a8bb12e55a933af08
Jason Grosman <jgrosman@npr.org>
2014-06-18 15:41:51 -0400

    Adding helper functions for creating your own pager, including total pages, total items, and offset calculations for next and previous

--- src/Pmp/Sdk/PageIterator.php
+++ src/Pmp/Sdk/PageIterator.php
@@ -27,7 +27,65 @@ class PageIterator
             return 0;
         }
     }
-
+    
+    /**
+     * Number of total results availabe
+     * @return int
+     */
+    public function totalItems() {
+    	$links = $this->_navigationLinks->rels(array('self'));
+    	if (!empty($links[0])) {
+    		return $links[0]->totalitems;
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    /**
+     * Number of total pages availabe
+     * @return int
+     */
+    public function totalPages() {
+    	$links = $this->_navigationLinks->rels(array('self'));
+    	if (!empty($links[0])) {
+    		return $links[0]->totalpages;
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    /**
+     * The offset to use to get the next page of items
+     * Returns -1, if at the end of the results
+     * @return int
+     */
+    public function nextOffset() {
+    	$links = $this->_navigationLinks->rels(array('next'));
+    	if (!empty($links[0])) {
+    		$nextPageNum =  $links[0]->pagenum;
+    		$numItemsReturned = count($this->_items);
+    		return ($nextPageNum - 1) * $numItemsReturned;
+    	} else {
+    		return -1;
+    	}
+    }
+    
+    /**
+     * The offset to use to get the previous page of items
+     * Returns -1, if at the end of the results
+     * @return int
+     */
+    public function previousOffset() {
+    	$links = $this->_navigationLinks->rels(array('prev'));
+    	if (!empty($links[0])) {
+    		$prevPageNum =  $links[0]->pagenum;
+    		$numItemsReturned = count($this->_items);
+    		return ($prevPageNum - 1) * $numItemsReturned;
+    	} else {
+    		return -1;
+    	}
+    }
+    
     /**
      * Items on the current page
      * @return CollectionDocJsonItems
