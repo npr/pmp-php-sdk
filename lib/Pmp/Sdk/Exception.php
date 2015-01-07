52619a8815b1d5f96ac079aff42fee651d274cbd
cavis <ryancavis@gmail.com>
2015-01-07 10:21:30 -0700

    Init auth-user class for managing user-clients

--- lib/Pmp/Sdk/Exception.php
+++ lib/Pmp/Sdk/Exception.php
@@ -4,7 +4,16 @@ namespace Pmp\Sdk;
 
 class Exception extends \Exception
 {
+    private $httpCode = null;
     private $details = array();
+
+    public function getHttpCode() {
+      return $this->httpCode;
+    }
+
+    public function setHttpCode($code) {
+      $this->httpCode = $code;
+    }
 
     public function getDetails() {
         return $this->details;
@@ -13,4 +22,4 @@ class Exception extends \Exception
     public function setDetails(array $details) {
         $this->details = $details;
     }
-} 
+}
